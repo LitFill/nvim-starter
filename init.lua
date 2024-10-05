c5606ed0c114ba17768733c1c792ba186f8208a7
LitFill <marrazzy54@gmail.com>
2024-10-06 01:26:37 +0700

    feat(plugins)!: add Oil and Transparent
    1. Menambah Oil.nvim dan mengatur opsinya
    2. Menambah Transparent.nvim

--- init.lua
+++ init.lua
@@ -1,2 +1,63 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("oil").setup({
+  default_file_explorer = true,
+  columns = {
+    "size",
+    "mtime",
+    "icon",
+  },
+  win_options = {
+    wrap = false,
+    signcolumn = "no",
+    cursorcolumn = false,
+    foldcolumn = "0",
+    spell = false,
+    list = false,
+    conceallevel = 3,
+    concealcursor = "nvic",
+  },
+  lsp_file_methods = {
+    enabled = true,
+    timeout_ms = 1000,
+    autosave_changes = false,
+  },
+  constrain_cursor = "editable",
+  watch_for_changes = true,
+  view_options = {
+    show_hidden = false,
+    is_hidden_file = function(name, _)
+      return vim.startswith(name, ".")
+    end,
+    natural_order = true,
+    case_insensitive = true,
+    sort = {
+      { "type", "asc" },
+      { "name", "asc" },
+    },
+  },
+  preview = {
+    -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_width and max_width can be a single value or a list of mixed integer/float types.
+    -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+    max_width = 0.9,
+    -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+    min_width = { 40, 0.4 },
+    -- optionally define an integer/float for the exact width of the preview window
+    width = nil,
+    -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_height and max_height can be a single value or a list of mixed integer/float types.
+    -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+    max_height = 0.9,
+    -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+    min_height = { 5, 0.1 },
+    -- optionally define an integer/float for the exact height of the preview window
+    height = nil,
+    border = "rounded",
+    win_options = {
+      winblend = 0,
+    },
+    update_on_cursor_moved = true,
+  },
+})
